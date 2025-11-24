90eb3beba078693c94a23b780582e504e5482933
Alex Maleknia <alex.maleknia@inria.fr>
2025-11-24 13:25:48 +0100

    exp done!

--- paper1_try1.py
+++ paper1_try1.py
@@ -17,14 +17,14 @@ n = 20
 d_in = 2
 d_out = 1
 LOO_L = []  #losses on the two models with LOO
-IF_L = []   ##losses on the two models with IFs
+IF_L = []   #losses on the two models with IFs
 device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
 print(f"Using {device} device")
 
 # Generate sample data (20 samples, 2 features)
 X = np.random.randn(n, d_in).astype(np.float32)   # feature matrix (20 × 2)
-y = (X[:, 0] + 0.3*X[:, 1] -1 > 0).astype(np.float32)  # binary labels based on a simple rule
-
+e = np.random.normal(0, 0.1, n).astype(np.float32)  # data error
+y = (X[:, 0] + 0.3*X[:, 1] -0.5 + e[:] > 0).astype(np.float32)  # binary labels based on a simple rule
 
 
 # Define MLP
@@ -63,14 +63,14 @@ def train_MLP(X,y,MAX_IT=500):
 
 
 def train_LR(X,y):
-    # Train Logistic Regression model
+    """Train Logistic Regression model"""
     model = LogisticRegression()
     model.fit(X, y)
     return model
 
 
 def train_lR(X,y):
-    # Train Logistic Regression model
+    """Train Linear Regression model"""
     model = LinearRegression()
     model.fit(X, y)
     return model
@@ -84,25 +84,27 @@ def train_models(X,y):
     return train_LR(X,y), train_MLP(X_t,y_t)
 
 
+def criterion(z): return sum([(a-b)**2/2 for (a,b) in z])/len(z)
+
+
 def eval_models(X,y,L):
     # Train-test split
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.3, random_state=42
-    )
-    lR,M = train_models(X_train,y_train)
-    criterion = lambda z : sum([(a-b)**2/2 for (a,b) in z])/len(z)
+    #X_train, X_test, y_train, y_test = train_test_split(
+    #    X, y, test_size=0.3, random_state=42
+    #)
+    lR,M = train_models(X,y)
 
     # Predictions
-    preds_lR = lR.predict(X_test)
+    preds_lR = lR.predict(X)
     #print("\nPredictions:", preds_lR)
     #print("True labels:", y)
 
     with torch.no_grad():
-        preds_M = (M(torch.tensor(X_test)) > 0.5).float().squeeze()
+        preds_M = (M(torch.tensor(X)) > 0.5).float().squeeze()
     #print("\nPredictions:", preds_M.numpy())
     #print("True labels:", y_test)
 
-    L.append([criterion(list(zip(preds_lR, y_test))).item(), criterion(list(zip(preds_M, y_test))).item()])
+    L.append([criterion(list(zip(preds_lR, y))).item(), criterion(list(zip(preds_M, y))).item()])
     return L
 
 
@@ -145,40 +147,51 @@ def L_IF_MLP(i, X, y):
         return out
 
     # ---- functional forward pass using your module architecture ----
-    def functional_forward(theta, X):
+    def functional_forward(theta):
         W1, W2 = vector_to_params(theta)  # fc1.weight, fc2.weight
         h = torch.tanh(X @ W1.t())
         out = torch.sigmoid(h @ W2.t())
         return out
+    
+    external_grad = torch.tensor([1.]*n)
+    external_grad = external_grad.view(-1, 1)
+    evaluation = functional_forward(theta0)
+    evaluation.backward(gradient=external_grad)
+    g = np.sqrt((sum([(a-b)**2/2 for (a,b) in list(zip(M(X),y))])).detach().numpy()).item()*theta0.grad
 
     # ---- loss as a function of theta ----
     def loss_fn(theta):
-        preds = functional_forward(theta, X)
-        return torch.nn.functional.mse_loss(preds, y)
+        preds = functional_forward(theta)
+        return criterion([a,b for (preds, y)])
 
     # Compute Hessian
-    H = torch.autograd.functional.hessian(loss_fn, theta0)
+    H = torch.autograd.functional.hessian(loss_fn, theta0).detach().numpy()
+    g = g.detach().numpy()
 
-    print("Is Hessian zero?", torch.all(H == 0).item())
+    #print("Is Hessian zero?", torch.all(H == 0).item())
+
+    return g.transpose() @ np.linalg.solve(H,g)
 
 
 def visualize_result(title, IFL, LOOL):
+    plt.close("all")
     max_abs = np.max([np.abs(IFL), np.abs(LOOL)])
     min_, max_ = -max_abs * 1.1, max_abs * 1.1
     plt.rcParams['figure.figsize'] = 6, 5
+    plt.scatter(IFL.pop(0), LOOL.pop(0), zorder=2, s=10, c='red') #error without taking out any point
     plt.scatter(IFL, LOOL, zorder=2, s=10)
     plt.title(title)
     plt.xlabel('IF loss diff')
     plt.ylabel('LOO loss diff')
     range_ = [min_, max_]
-    plt.plot(range_, range_, 'k-', alpha=0.2, zorder=1)
+    plt.plot(range_, range_, 'k-', alpha=0.2, zorder=1)  #retta (zorder=1 vuol dire che viene stampata per prima)
     plt.xlim(min_, max_)
     plt.ylim(min_, max_)
-
+    plt.savefig("high_res_figure.png", dpi=600)
     plt.show()
 
-Z = eval_LOO(X,y)
-#print(L_IF_lR(2,X,y))
+if __name__ == '__main__':
+    Z = eval_LOO(X,y)
 
-#L_IF_MLP(1,X,y)
-visualize_result('IF vs LOO (Linear Regression)', [Z[i][0] for i in range(len(X))], [L_IF_lR(i,X,y) for i in range(len(X))])
+    #visualize_result('IF vs LOO (Linear Regression)', [Z[i][0] for i in range(len(X))], [L_IF_lR(i,X,y) for i in range(len(X))])
+    visualize_result('IF vs LOO (MLP)', [Z[i][1] for i in range(len(X))], [L_IF_MLP(i,X,y) for i in range(len(X))])
